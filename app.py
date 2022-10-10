290f032ed1a360a33840259213c03f5002e73a0a
lcalmbach <lcalmbach@gmail.com>
2022-10-10 16:49:27 +0200

    Initial Version

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ APP_INFO = f"""<div style="background-color:powderblue; padding: 10px;border-rad
 
 def_options_days = (1, 365)
 def_options_hours = (0, 23)
+def_options_weeks = (1, 53)
 
 @st.cache
 def get_data():
@@ -43,7 +44,9 @@ def get_data():
         df['day'] = df['zeitpunkt'].dt.day
         df['hour_date'] = pd.to_datetime(df[['year', 'month', 'day', 'hour']])
         df['day'] = df['zeitpunkt'].dt.dayofyear
-        df['zeit'] = df['zeitpunkt'].dt.strftime('%H:%M:%S')
+        df['zeit'] = df['zeitpunkt'].dt.strftime('%H:%M')
+        df['day_of_week'] = df['zeitpunkt'].dt.dayofweek
+        df['week_time'] = df['day_of_week'] + df['hour']/24 + df['zeitpunkt'].dt.minute / (24*60)
         return df
 
     df = pd.DataFrame()
@@ -78,7 +81,7 @@ def consumption_year(df):
 
     def get_filtered_data(df):
         with st.sidebar.expander('🔎 Filter', expanded=True):
-            sel_days = st.slider('Auswahl Tag im Jahr', min_value=1, max_value=365, value=def_options_days)
+            sel_days = st.slider('Auswahl Tag im Jahr', min_value=1, max_value=max(def_options_days), value=def_options_days)
             st.markdown(get_interval_dates(sel_days))
             sel_years = st.multiselect('Auswahl Jahre', options=range(2019,2023), help="keine Auswahl = alle Jahre")
         if sel_days != def_options_days:
@@ -101,7 +104,7 @@ def consumption_day(df):
         settings = {'x': 'zeit', 'x_dt': 'O', 'y':'bruttolastgang', 'color':'year:O', 'tooltip':['year','zeit', 'bruttolastgang'], 
                 'width':800,'height':400, 'title': 'Tagesganglinie, mittlerer Viertelstunden-Verbrauch'}
         
-        settings['x_labels'] = [f"{str(x).rjust(2, '0')}:00:00" for x in range(0,23+1)]
+        settings['x_labels'] = [f"{str(x).rjust(2, '0')}:00" for x in range(0,23+1)]
         plots.line_chart(df, settings)
 
     def get_filtered_data(df):
@@ -125,28 +128,30 @@ def consumption_day(df):
 
 def consumption_week(df):
     def show_plot(df):
-        settings = {'x': 'zeit', 'x_dt': 'O', 'y':'bruttolastgang', 'color':'year:O', 'tooltip':['year','zeit', 'bruttolastgang'], 
-                'width':800,'height':400, 'title': 'Tagesganglinie, mittlerer Viertelstunden-Verbrauch'}
+        settings = {'x': 'week_time', 'x_dt': 'O', 'y':'bruttolastgang', 'color':'year:O', 'tooltip':['year','week_time', 'bruttolastgang'], 
+                'width':800,'height':400, 'title': 'Wochenganglinie, mittlerer Viertelstunden-Verbrauch'}
         
-        settings['x_labels'] = [f"{str(x).rjust(2, '0')}:00:00" for x in range(0,23+1)]
+        #settings['x_labels'] = {0: 'So',1:'Mon',2:'Di',3:'Mi',4:'Do',5:'Fr',6:'Sa'}
+        settings['x_labels'] = ['Mon','Di','Mi','Do','Fr','Sa','So']
+        settings['x_labels'] = [0,1,2,3,4,5,6]
         plots.line_chart(df, settings)
 
     def get_filtered_data(df):
         with st.sidebar.expander('🔎 Filter', expanded=True):
-            sel_days = st.slider('Auswahl Tag im Jahr', min_value=1, max_value=365, value=def_options_days)
-            st.markdown(get_interval_dates(sel_days))
+            sel_weeks = st.slider('Auswahl Kalenderwochen', min_value=1, max_value=max(def_options_weeks), value=def_options_weeks)
+            st.markdown(get_interval_dates(sel_weeks))
             
             sel_years = st.multiselect('Auswahl Jahre', options=range(2019,2023), help="keine Auswahl = alle Jahre")
-            if sel_days != def_options_days:
-                df = df[(df['day'] >= sel_days[0]) & (df['day'] <= sel_days[1])]
+            if sel_weeks != def_options_weeks:
+                df = df[(df['week'] >= sel_weeks[0]) & (df['week'] <= sel_weeks[1])]
         if sel_years:
             df = df[df['year'].isin(sel_years)]
         return df
 
     df_time = get_filtered_data(df.copy())
     df_time['bruttolastgang'] = df_time['bruttolastgang'] * 1000
-    fields = ['year', 'zeit', 'bruttolastgang']
-    agg_fields = ['year','zeit']
+    fields = ['year', 'week_time', 'bruttolastgang']
+    agg_fields = ['year','week_time']
     df_time = df_time[fields].groupby(agg_fields).mean().reset_index()
     show_plot(df_time)
 
